db8e701f09caa408739248afcc41beaac7681c23
Nitinegoro <pkpvicky@gmail.com>
2017-08-03 14:51:01 -0700

    prototype single page

--- public/theme/js/main.js
+++ public/theme/js/main.js
@@ -1,12 +1,18 @@
-$(function() {
-    var div = $(".sticky");
-    $(window).scroll(function() {    
-        var scroll = $(window).scrollTop();
-    
-        if (scroll >= 100) {
-            div.removeClass('hidden-lg').addClass("visible-lg");
-        } else {
-            div.removeClass("visible-lg").addClass('hidden-lg');
-        }
-    });
+$(document).ready(function() {
+  var $navbar = $("#sticky-navbar");
+  
+  AdjustHeader(); // Incase the user loads the page from halfway down (or something);
+  $(window).scroll(function() {
+      AdjustHeader();
+  });
+  
+  function AdjustHeader(){
+    if ($(window).scrollTop() > 80) {
+      if (!$navbar.hasClass("navbar-fixed-top")) {
+        $navbar.addClass("navbar-fixed-top");
+      }
+    } else {
+      $navbar.removeClass("navbar-fixed-top");
+    }
+  }
 });
